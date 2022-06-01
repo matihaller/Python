7b3e805dea8f1a6ed6f6639088d33b17a86e4c7e
Emi Mati <matiashallerpe@gmail.com>
2022-06-01 13:07:49 -0300

    Commit Leccion 2

---
+++ Leccion2/main.py
@@ -0,0 +1,30 @@
+# Sentencia if/else
+'''condicion = 'Hola'
+if condicion:
+    print('Condicion verdadera')
+elif condicion == False:
+    print('Condicion falsa')
+else:
+    print('Condicion falsa')
+'''
+
+# Conversion de numero a texto
+num = int(input('Digite un numero en el rango del 1 al 3: '))
+numTexto = ''
+if num == 1:
+    numTexto = 'Numero uno'
+elif num == 2:
+    numTexto = 'Numero dos'
+elif num == 3:
+    numTexto = 'Numero tres'
+else:
+    numTexto = 'Has ingresado un numero fuera de rango'
+print(f'El numero ingresado es: {num} - {numTexto}')
+
+
+
+
+
+
+
+
