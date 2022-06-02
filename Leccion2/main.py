8da5fbfe74c0246c37cafab307e0fe1f6a2149ce
Emi Mati <matiashallerpe@gmail.com>
2022-06-02 12:03:16 -0300

    Commit Leccion 2

--- Leccion2/main.py
+++ Leccion2/main.py
@@ -9,17 +9,27 @@ else:
 '''
 
 # Conversion de numero a texto
-num = int(input('Digite un numero en el rango del 1 al 3: '))
-numTexto = ''
-if num == 1:
-    numTexto = 'Numero uno'
-elif num == 2:
-    numTexto = 'Numero dos'
-elif num == 3:
-    numTexto = 'Numero tres'
-else:
-    numTexto = 'Has ingresado un numero fuera de rango'
-print(f'El numero ingresado es: {num} - {numTexto}')
+# num = int(input('Digite un numero en el rango del 1 al 3: '))
+# numTexto = ''
+# if num == 1:
+#     numTexto = 'Numero uno'
+# elif num == 2:
+#     numTexto = 'Numero dos'
+# elif num == 3:
+#     numTexto = 'Numero tres'
+# else:
+#     numTexto = 'Has ingresado un numero fuera de rango'
+# print(f'El numero ingresado es: {num} - {numTexto}')
+
+# ctrl + / para comentar
+
+condicion = False
+# if condicion:
+#     print('Condicion Verdadera')
+# else:
+#     print('Condicion falsa')
+
+print('Condicion verdadera') if condicion else print('Condicion falsa')
 
 
 
